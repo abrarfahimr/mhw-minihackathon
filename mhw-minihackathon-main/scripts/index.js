5df1f2797341bda4a2ec0d6d97827672b578daea
Michael Rideout <rideout.m@hotmail.com>
2022-08-09 14:51:48 -0400

    parse data

--- mhw-minihackathon-main/scripts/index.js
+++ mhw-minihackathon-main/scripts/index.js
@@ -11,7 +11,14 @@ fetch("https://mhw-db.com/monsters")
   .then((response) => response.json())
   .then((monsters) => {
     console.log(monsters);
+
     monsters.forEach((el) => {
+      let name = el.name;
+      let type;
+      let species;
+      let description;
+      name = el.name;
+
       console.log(el.name, el.type, el.species, el.description);
     });
   });
